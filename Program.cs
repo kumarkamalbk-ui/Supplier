6cd3eb1f5884cea7ae551ff28b603a70ac96db84
Kammal Kumar <kumarkamalbk@gmail.com>
2025-09-01 12:01:59 +0530

    Practice code

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter radius of circle: ");
+        int radius = int.Parse(Console.ReadLine());
+        
+
+        switch (grade)
+        {
+            case 'A':
+                Console.WriteLine("Execellent");
+                break;
+
+            case 'B':
+                Console.WriteLine("Good");
+                break;
+
+            case 'C':
+                Console.WriteLine("Average");
+                break;
+
+            case 'D':
+                Console.WriteLine("Below average");
+                break;
+
+            case 'F':
+                Console.WriteLine("Fail");
+                break;
+
+            default:
+                Console.WriteLine("Invalid Input");
+                break;
+
+        }
+    }
+}
+
+
+        
+
+        
